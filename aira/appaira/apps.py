db1e4b820d96df04758998ae24fcdce7447b33b6
AliceRodgers <157533505+AliceRodgers@users.noreply.github.com>
2024-04-08 17:55:25 +0100

    Delete aira directory

--- aira/appaira/apps.py
+++
@@ -1,8 +0,0 @@
-"""module docstring for apps.py"""
-from django.apps import AppConfig
-
-
-class AppairaConfig(AppConfig):
-    """configuration class for airaapp"""
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'appaira'
