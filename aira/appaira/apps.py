f7e0df03a3b4d21a29f15001de4bbc143d7eb7cf
EC2 Default User <ec2-user@ip-172-31-17-209.eu-west-1.compute.internal>
2024-04-08 12:25:16 +0000

    new

---
+++ aira/appaira/apps.py
@@ -0,0 +1,8 @@
+"""module docstring for apps.py"""
+from django.apps import AppConfig
+
+
+class AppairaConfig(AppConfig):
+    """configuration class for airaapp"""
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'appaira'
