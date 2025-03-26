477dc844f42e330d096821b15f92f1d3e8dda763
AliceRodgers <157533505+AliceRodgers@users.noreply.github.com>
2025-03-26 15:12:40 +0000

    Update settings.py

--- aira/settings.py
+++ aira/settings.py
@@ -26,9 +26,8 @@ SECRET_KEY = 'django-insecure-&l%pt$!q9x%5z%x*!^#i1g1gw5u^nes03%mjqgp7z8f(gp66gm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['aira23210362-env.eba-kptptmqy.eu-west-1.elasticbeanstalk.com',
-'c06dd58bd73b4a709667c945a287106e.vfs.cloud9.eu-west-1.amazonaws.com']
-CSRF_TRUSTED_ORIGINS=['https://c06dd58bd73b4a709667c945a287106e.vfs.cloud9.eu-west-1.amazonaws.com']
+ALLOWED_HOSTS = ['airarepo.onrender.com']
+CSRF_TRUSTED_ORIGINS=['https://airarepo.onrender.com']
 # Application definition
 
 INSTALLED_APPS = [
